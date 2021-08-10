644e7c3432e40ceab99c57ff16f454aa12a9bfbc
mint25mt <yuzuzumint25mt@gmail.com>
2021-08-11 02:46:22 +0900

    fix message

--- corona/corona.go
+++ corona/corona.go
@@ -65,7 +65,7 @@ func DisPlayTodayCorona(filename string, date string) string {
 			retval += displayList[line[1]] + "の感染者数は" + line[2] + "人です\n"
 		}
 	}
-	retval += date + "の報告"
+	retval += date + "の報告でした\n"
 	retval += "感染予防をして過ごしましょう\n"
 	return retval
 }
