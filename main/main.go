93cbb4ca959fdcead8c745af06c2a5cb3edd04b0
mint25mt <yuzuzumint25mt@gmail.com>
2021-08-10 21:06:37 +0900

    fix message

--- main/main.go
+++ main/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -11,14 +12,13 @@ import (
 var (
 	Token      = os.Getenv("DISCORD_BOT_TOKEN")
 	BotName    = "874558469708075019"
-	stopBot    = make(chan bool)
 	vcsession  *discordgo.VoiceConnection
 	HelloWorld = "!helloworld"
 )
 
 func main() {
 	//Discordのセッションを作成
-	discord, err := discordgo.New()
+	discord, err := discordgo.New("Bot ", Token)
 	discord.Token = Token
 	if err != nil {
 		fmt.Println("Error logging in")
@@ -27,33 +27,38 @@ func main() {
 
 	discord.AddHandler(onMessageCreate) //全てのWSAPIイベントが発生した時のイベントハンドラを追加
 	// websocketを開いてlistening開始
+	// In this example, we only care about receiving message events.
+	discord.Identify.Intents = discordgo.IntentsGuildMessages
+
+	// Open a websocket connection to Discord and begin listening.
 	err = discord.Open()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("error opening connection,", err)
+		return
 	}
-	defer discord.Close()
 
-	fmt.Println("Listening...")
-	<-stopBot //プログラムが終了しないようロック
-	return
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc //プログラムが終了しないようロック
+
+	discord.Close()
 }
 
 func onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.Bot {
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
 		return
 	}
-	switch m.Content {
-	case "!HelloWorld":
-		s.ChannelMessageSend(m.ChannelID, "nyanyanyanya!!!")
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
 	}
-}
 
-//メッセージを送信する関数
-func sendMessage(s *discordgo.Session, channelID string, msg string) {
-	_, err := s.ChannelMessageSend(channelID, msg)
-
-	log.Println(">>> " + msg)
-	if err != nil {
-		log.Println("Error sending message: ", err)
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
 	}
 }
