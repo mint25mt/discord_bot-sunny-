3e9c154f7c3256e9bfa34400479431eaecd27c61
HighPon <mjoppaida@icloud.com>
2021-08-17 18:41:19 +0900

    display cat with command

--- bot/bot.go
+++ bot/bot.go
@@ -50,13 +50,13 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "ping" {
-		s.ChannelMessageSend(m.ChannelID, "Pong!")
-	}
+	f, err := os.Open("./bot/cat.jpg")
 
+	if err != nil {
+		fmt.Println("error")
+	}
 	// If the message is "pong" reply with "Ping!"
-	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	if m.Content == "utu" {
+		s.ChannelFileSendWithMessage(m.ChannelID, "元気だせにゃ〜", "cat.jpg", f)
 	}
 }
